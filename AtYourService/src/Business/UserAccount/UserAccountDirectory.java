2cbaae230a2504fea3765a7f15c82fbb00259070
dabhi-v <dabhi.v@northeastern.edu>
2022-12-09 16:50:03 -0500

    Initial commits of UI

---
+++ AtYourService/src/Business/UserAccount/UserAccountDirectory.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.UserAccount;
+
+import Business.Employee.Employee;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vikas
+ */
+public class UserAccountDirectory {
+    
+    private ArrayList<UserAccount> userAccountList;
+
+    public UserAccountDirectory() {
+        userAccountList = new ArrayList();
+    }
+
+    public ArrayList<UserAccount> getUserAccountList() {
+        return userAccountList;
+    }
+    
+    public UserAccount authenticateUser(String username, String password){
+        for (UserAccount ua : userAccountList)
+            if (ua.getUsername().equals(username) && ua.getPassword().equals(password)){
+                return ua;
+            }
+        return null;
+    }
+    
+    public UserAccount createUserAccount(String username, String password, Employee employee, Role role, String roleName){
+        UserAccount userAccount = new UserAccount();
+        userAccount.setUsername(username);
+        userAccount.setPassword(password);
+        userAccount.setEmployee(employee);
+        userAccount.setRole(role);        
+        userAccount.setRoleName(roleName);
+        userAccountList.add(userAccount);
+        return userAccount;
+    }
+    
+    public Boolean deleteUser(String name) {
+        for(int i = 0; i < userAccountList.size(); i ++) {
+            if(userAccountList.get(i).getEmployee().getName().equals(name)) {
+                userAccountList.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean checkIfUsernameIsUnique(String username){
+        for (UserAccount ua : userAccountList){
+            if (ua.getUsername().equals(username))
+                return false;
+        }
+        return true;
+    }
+}
